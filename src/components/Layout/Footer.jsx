c931e9ed9d85aa36d8bb34c0ee1045269591cdc2
shahab khan <hussainmuhammad098123123@gmail.com>
2024-02-01 21:44:35 -0800

    commit

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -7,16 +7,16 @@ import link from '../../assets/svg/linkdin.svg'
 const Footer = () => {
   return (
     <div className="bg-[#F8F9FA] py-16 mt-40">
-      <div className=" md:flex block items-center justify-between md:mx-24 mx-4">
-        <img src={footer} alt="" />
-        <div>
+      <div className=" md:flex block items-center md:justify-between md:mx-24 mx-4">
+        <img className=" mx-auto" src={footer} alt="" />
+        <div className=" flex flex-col items-center justify-center">
           <p className=" text-[24px] font-semibold py-3">Head Office</p>
-          <p className=" text-[14px] font-normal">
+          <p className=" text-[14px] font-normal text-center">
             Office # 09,2nd Floor Ali Tower,Main University Road Peshawar,
             Pakistan
           </p>
         </div>
-        <div>
+        <div className=" flex flex-col items-center justify-center">
           <h1 className=" text-[24px] font-semibold py-3">Social Links</h1>
           <ul className=" md:flex sm:flex block gap-4 items-center">
             <a className=" flex items-center gap-3" href="/">
