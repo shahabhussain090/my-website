94accc1fbbaf658867441f2cfbb1d47ec669cff4
shahab khan <hussainmuhammad098123123@gmail.com>
2024-02-03 04:34:41 -0800

    recent

--- src/components/Home/RecentWork.jsx
+++ src/components/Home/RecentWork.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { recentData } from "../Data/data";
-import arrow from '../../assets/svg/Arrow.svg'
+import arrow from "../../assets/svg/Arrow.svg";
 import { Link } from "react-router-dom";
 const RecentWork = () => {
-
   return (
-    <div
-      className="mt-32 md:mx-24 mx-4"
-    >
+    <div className="mt-32 md:mx-24 mx-4">
       <h1 className="text-[40px] font-bold text-center">Recent Work</h1>
 
       <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 grid-cols-1 gap-4 mt-12">
@@ -32,7 +29,9 @@ const RecentWork = () => {
                 {item.desc}
               </p>
               <div className="flex items-center gap-1 mt-4">
-                <button className="text-[#14A2F1] text-[20px] font-medium">{item.btn}</button>
+                <button className="text-[#14A2F1] text-[20px] font-medium">
+                  {item.btn}
+                </button>
                 <img src={arrow} alt="" />
               </div>
             </div>
@@ -41,7 +40,8 @@ const RecentWork = () => {
       </div>
 
       <div className="flex items-center justify-center ">
-        <Link to='/recent'
+        <Link
+          to="/recent"
           className="bg-[#14A2F1] text-white mt-14 py-3 px-7 hover:bg-[#4889ac]"
         >
           View More Case Studies
